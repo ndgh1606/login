6e90d3f28c315f8fc86d8f4813b57c1deb45e615
ndgh <huyndggcs210489@fpt.edu.vn>
2024-03-05 13:35:21 +0700

    Login Section

---
+++ src/Components/LoginSignup/Loginsignup.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react'
+import './LoginSignup.css';
+import user_icon from '../Assets/person.png'
+import email_icon from '../Assets/email.png'
+import password_icon from '../Assets/password.png'
+export const Loginsignup = () => {
+
+    const [action, setAction] = useState('Sign up');
+  return (
+    <div className='container'>
+        <div className='header'>
+            <div className="text">{action}</div>
+            <div className="underline"></div>
+        </div>
+        <div className="inputs">
+            <div className="input">
+                <img src={user_icon} alt="" />
+                <input type="text" placeholder='Name'/>
+            </div>
+            <div className="input">
+                <img src={email_icon} alt="" />
+                <input type="email" placeholder='Email' />
+            </div>
+            <div className="input">
+                <img src={password_icon} alt="" />
+                <input type="password" placeholder='Passoword' />
+            </div>
+        </div>
+        <div className="forgot-password">Lost Password? <span>Click Here</span></div>
+        <div className="submit-container">
+            <div className={action==="Login"?'submit gray':'submit'}>Sign Up</div>
+            <div className={action==='Sign Up'?'submit gray':'submit'}>Login</div>
+        </div>
+    </div>
+  )
+}
+export default Loginsignup
